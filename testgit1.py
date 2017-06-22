0e7829875326e60abe57e26de0b3d507b95097f7
norm <norm0413@foxmail.com>
2017-06-22 10:25:13 +0800

    'adding'

--- testgit1.py
+++ testgit1.py
@@ -4,6 +4,19 @@ noting but still test
 
 import time
 
-now = time.asctime()
-print now,
-print 'is that what you want?'
+def test0():
+    now = time.asctime()
+    print now,
+    print 'is that what you want?'
+
+
+def test_sys_exit():
+
+    for _ in range(10):
+        print 'Hello conda!'
+
+
+if(__name__ == '__main__'):
+    import sys
+    sys.exit(test_sys_exit())
+    print 'done!'
